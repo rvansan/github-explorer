d1ed895fcb0022bf63769ef7ee6dcce818ae2c05
Rafael Vansan <rafaelvansan@MacBook-Air-de-Rafael.local>
2021-05-13 13:52:38 -0300

    01- Primeiro componente React

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 
-export function App(){
-    return <h1>Test 4</h1>
+export function App() {
+    return <RepositoryList/>
 }
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,24 @@
+export function RepositoryList(){
+    return (
+        <section className="repository-list">
+            <h1>Lista de Repositorios</h1>
+            <ul>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">Acessar Repositório</a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">Acessar Repositório</a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">Acessar Repositório</a>
+                </li>
+            </ul>
+        </section>
+    );
+}
